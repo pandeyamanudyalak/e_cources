fa0a62a13ec29b208d93fab5cc660168cb3b2f85
aman pandey <amanpandeyudyalak@gmail.com>
2024-03-11 21:24:02 +0530

    'blog_module_update'

--- e_cources/settings.py
+++ e_cources/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'e_app'
+    'e_app',
+    'blog'
 ]
 
 MIDDLEWARE = [
--- e_cources/urls.py
+++ e_cources/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('e_app.urls'))
+    path('',include('e_app.urls')),
+    path('blog/',include('blog.urls'))
     ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
